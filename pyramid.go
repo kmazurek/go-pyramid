5a9012fe635d4def9d248f71cb0cca712816cfb0
Jakub Mazurek <kuba@uber.com>
2016-08-22 21:11:34 +0200

    Implemented checking tiles above a given tile

--- pyramid.go
+++ pyramid.go
@@ -6,7 +6,8 @@ type Tile struct {
 }
 
 type Pyramid struct {
-	tileLevels map[int8][][]*Tile
+	numberOfLevels int8
+	tileLevels     map[int8][][]*Tile
 }
 
 func (pyramid *Pyramid) GetAvailableTiles() []Tile {
@@ -20,16 +21,17 @@ func NewPyramid(numberOfLevels int8) (*Pyramid, int) {
 	result := new(Pyramid)
 	tileCounter := 0
 
+	result.numberOfLevels = numberOfLevels
 	result.tileLevels = make(map[int8][][]*Tile)
 
-	for int8 i = 1; i <= numberOfLevels; i++ {
+	for i := int8(1); i <= numberOfLevels; i++ {
 		result.tileLevels[i] = make([][]*Tile, i, i)
 		currentLevel := result.tileLevels[i]
 
-		for j := 0; j < i; j++ {
+		for j := int8(0); j < i; j++ {
 			currentLevel[j] = make([]*Tile, i, i)
 
-			for k := 0; k < i; k++ {
+			for k := int8(0); k < i; k++ {
 				currentLevel[j][k] = new(Tile)
 				tileCounter++
 			}
@@ -39,9 +41,40 @@ func NewPyramid(numberOfLevels int8) (*Pyramid, int) {
 	return result, tileCounter
 }
 
-func checkTile(tile Tile, level int8) bool {
+func (pyramid *Pyramid) checkTile(row, column int, levelKey int8) bool {
 	// Tile checking logic goes here
+	currentLevel := pyramid.tileLevels[levelKey]
+	tile := currentLevel[row][column]
+
+	if levelKey == 1 {
+		return !tile.taken
+	} else {
+		if hasTilesAbove(row, column, levelKey, pyramid) {
+			return false
+		}
+	}
+
 	return true
+}
+
+func hasTilesAbove(row, column int, level int8, pyramid *Pyramid) bool {
+	levelAbove := pyramid.tileLevels[level-1]
+
+	for i := row; i > i-2; i-- {
+		for j := column; j > j-2; j-- {
+			if isPermittedIndex(i, level-1) && isPermittedIndex(j, level-1) {
+				if levelAbove[i][j].taken == false {
+					return true
+				}
+			}
+		}
+	}
+
+	return false
+}
+
+func isPermittedIndex(index int, level int8) bool {
+	return index >= 0 && index < int(level)
 }
 
 func dequeue(collection *[]Tile) Tile {
