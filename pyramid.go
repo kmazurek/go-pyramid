4349fc6a499dda5e83abdd30cb0d08974eefc1a4
Jakub Mazurek <kuba@uber.com>
2016-08-22 02:46:59 +0200

    Initial commit

---
+++ pyramid.go
@@ -0,0 +1,52 @@
+package pyramid
+
+type Tile struct {
+	symbols [2][2]int8
+	taken   bool
+}
+
+type Pyramid struct {
+	tileLevels map[int][][]*Tile
+}
+
+func (pyramid *Pyramid) GetAvailableTiles() []Tile {
+	var availableTiles []Tile
+	// calls checkTile and fills in availableTiles
+
+	return availableTiles
+}
+
+func NewPyramid(numberOfLevels int) (*Pyramid, int) {
+	result := new(Pyramid)
+	tileCounter := 0
+
+	result.tileLevels = make(map[int][][]*Tile)
+
+	for i := 1; i <= numberOfLevels; i++ {
+		result.tileLevels[i] = make([][]*Tile, i, i)
+		currentLevel := result.tileLevels[i]
+
+		for j := 0; j < i; j++ {
+			currentLevel[j] = make([]*Tile, i, i)
+
+			for k := 0; k < i; k++ {
+				currentLevel[j][k] = new(Tile)
+				tileCounter++
+			}
+		}
+	}
+
+	return result, tileCounter
+}
+
+func checkTile(tile Tile) bool {
+	// Tile checking logic goes here
+	return true
+}
+
+func dequeue(collection *[]Tile) Tile {
+	length := len(*collection)
+	result := (*collection)[length-1]
+	*collection = (*collection)[:length-1]
+	return result
+}
